0f8fd720a10c6bb02082f2b1f3834f9e3f034019
robertreed4501 <robertreed4501@gmail.com>
2022-07-04 12:48:57 -0400

    moved an image, testing from laptop

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,7 @@ function App() {
       <Card />
       <Card />
       <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
     return (
         <header>
-            <img className="headerImg" src={"https://s.tmimgcdn.com/scr/66500/free-freelance-business-logo-template-logo-template_66571-original.jpg"} />
+            <img className="headerImg" src={"logo.svg"} />
             <button onClick={e => alert("you clicked a button")}>I'm a button</button>
             <button onClick={e => alert("i knew you'd try this one too")}>I'm another</button>
         </header>
