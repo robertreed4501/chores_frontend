399a1d5b1d47a6a9767aff1e40830d5c6c18c008
robertreed4501 <robertreed4501@gmail.com>
2022-09-06 14:26:40 -0400

    added private route to wrap all private routes in, added assign chore page

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,25 @@ import {Login} from "./pages/login";
 import {User as UserPage} from "./pages/User";
 import {Register} from "./pages/Register";
 import {Dashboard} from "./pages/Dashboard";
+import {PrivateRoutes} from "./PrivateRoutes";
+import {AssignChores} from "./pages/AssignChores";
 
 
 function App() {
-  return (
+  // @ts-ignore
+    return (
     <main className="app">
       <Routes>
-          <Route path="/" element={<TestElement/>} />
-          <Route path="/test" element={<OtherTest />} />
+          <Route element={<PrivateRoutes/>}>
+              <Route path="/" element={<TestElement/>} />
+              <Route path="/test" element={<OtherTest />} />
+
+              <Route path="/user" element={<UserPage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/assignchores" element={<AssignChores />} />
+          </Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/user" element={<UserPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
        <p>Hello?</p>
     </main>
