4d304b1f928618d76411f8b1ec95448f1d89206c
robertreed4501 <robertreed4501@gmail.com>
2022-09-06 14:25:46 -0400

    added cookies for auth, and if cookie present, get user from api and set auth, else to login

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,7 @@
 import { createContext, useState} from "react";
+import Cookies, {get} from "js-cookie";
+import axios from "axios";
+
 
 export const AuthContext = createContext({});
 
@@ -6,6 +9,25 @@ export const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
     // @ts-ignore
     const [auth, setAuth] = useState({});
+
+    const getResponse = async (key: string | undefined) => {
+        // @ts-ignore
+        await axios.get('http://10.0.0.18:8080/api/user', {withCredentials: false, headers:{'key': key}}).then(
+            (response) => {
+                setAuth(response.data.UserResponse)
+            }
+        ).catch((error) => console.log(error));
+    }
+
+    if (!auth) {
+        if (Cookies.get('key')) {
+            let key: string | undefined = Cookies.get('key');
+            getResponse(key).then(r => null);
+
+        }
+    }
+
+
 
     return (
         <AuthContext.Provider value={[auth, setAuth]}>
