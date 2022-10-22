f54b72a6258c8536b1e3f7ff7da3b3c53dfbde5a
robertreed4501 <robertreed4501@gmail.com>
2022-10-21 23:11:47 -0400

    added beginnings of homepage.  will rename later.

--- src/components/TestElement.tsx
+++ src/components/TestElement.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Container} from "react-bootstrap";
 
 function TestElement() {
     return (
-        <div>
-            <h1>This is the testing page</h1>
-    </div>
+        <Container className="m-2 shadow rounded-5 p-3">
+            <h1>Messy kids?  We can help!</h1>
+            <p>Don't just give your child chores, give them ChoresGalore!</p>
+        </Container>
 )
 }
 
