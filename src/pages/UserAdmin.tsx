76da136fa8fc67db379a9e97524fa885944abe64
robertreed4501 <robertreed4501@gmail.com>
2022-10-28 12:03:47 -0400

    added table for users instead of dropdown to select a user.

--- src/pages/UserAdmin.tsx
+++ src/pages/UserAdmin.tsx
@@ -1,9 +1,12 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {MouseEventHandler, ReactEventHandler, useContext, useEffect, useState} from "react";
 import axios from "../api/axios";
 import {AuthContext} from "../context/AuthProvider";
+
 import Cookies from "js-cookie";
 import Select from "react-select";
 import {AddUserModal} from "../components/AddUserModal";
+import {Button, Container, Table} from "react-bootstrap";
+import {EditUserModal} from "../components/EditUserModal";
 
 export const UserAdmin = () => {
 
@@ -27,8 +30,10 @@ export const UserAdmin = () => {
         firstName: string
     }
 
+
     const [userInfo, setUserInfo] = useState<UserInfo>();
     const [selectedUser, setSelectedUser] = useState<SelectedUser>();
+    // @ts-ignore
     const [userList, setUserList] = useState<UserList>();
     // @ts-ignore
     const [auth, setAuth] = useContext(AuthContext);
@@ -39,6 +44,7 @@ export const UserAdmin = () => {
         const response = await axios.get('/api/user/mygroup?id=' + auth.groupId, {withCredentials: false, headers:{'key': key}});
         // @ts-ignore
         setUserInfo(response.data);
+        console.log(JSON.stringify(userInfo))
     }
 
     const populateUserList = () => {
@@ -67,13 +73,47 @@ export const UserAdmin = () => {
 
     }
 
+    // @ts-ignore
+    const handleDelete = async (e) => {
+        const id = e.target.id;
+        const response = await axios.delete("/api/user/delete/" + id);
+        response.data === "user deactivated" ? await getUserInfo() : alert(response.data)
+    }
 
+    // @ts-ignore
     return(
-        <div>
+        <Container className="m-3 p-3 w-50">
             <Select
                 options={userList}
                 onChange={handleChange}
             />
+            <Table key="userTable" striped >
+                <thead>
+                <tr>
+                    <td>Name</td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+                </thead>
+                <tbody>
+                {userInfo?.map( user => {
+                    return(
+                        <tr>
+                            <td>{user.firstName + ' ' + user.lastName}</td>
+                            <td><EditUserModal
+                                id={user.id}
+                                firstName={user.firstName}
+                                lastName={user.lastName}
+                                email={user.email}
+                                role={user.role}
+                                getUserInfo={getUserInfo}
+                            /> </td>
+                            <td><Button id={user.id.toString()} onClick={handleDelete}>Delete</Button> </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </Table>
             {userInfo && selectedUser ?
                 userInfo.map(user =>
                     user.id === selectedUser.id ?
@@ -83,7 +123,7 @@ export const UserAdmin = () => {
                             <h4>{"API Key: " + user.key}</h4>
                         </div>:
                          null) : <h3>Select a user</h3>}
-            <AddUserModal />
-        </div>
+            <AddUserModal getUserInfo={getUserInfo}/>
+        </Container>
     );
 }
