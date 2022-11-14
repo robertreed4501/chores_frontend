82ce18a0372fbeda01dc65074d699485c6c14a0f
robertreed4501 <robertreed4501@gmail.com>
2022-11-14 18:50:26 -0500

    removed attempt at setting auth from provider

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,34 +1,66 @@
-import { createContext, useState} from "react";
+import {createContext, Dispatch, SetStateAction, useEffect, useState} from "react";
 import Cookies, {get} from "js-cookie";
-import axios from "axios";
+import axios from "../api/axios";
 
+type AuthType = {
+    id: number | null
+    firstName: string | null
+    lastName: string | null
+    email: string | null
+    role: string | null
+    key: string | null
+    groupId: number | null
+}
 
 export const AuthContext = createContext({});
 
 // @ts-ignore
 export const AuthProvider = ({ children }) => {
     // @ts-ignore
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState<AuthType>({
+        /*id: null,
+        firstName: null,
+        lastName: null,
+        email: null,
+        role: null,
+        key: null,
+        groupId: null*/
+    });
 
-    const getResponse = async (key: string | undefined) => {
+
+
+    /*const getResponse = async (key: string | undefined) => {
         // @ts-ignore
         await axios.get('http://10.0.0.18:8080/api/user', {withCredentials: false, headers:{'key': key}}).then(
             (response) => {
                 setAuth(response.data.UserResponse)
-                console.log(response.data.UserResponse)
+                console.log(response.data.UserResponse + " - user response from authProvider")
             }
         ).catch((error) => console.log(error));
     }
 
-    if (!auth) {
+    if (auth === undefined) {
         if (Cookies.get('key')) {
             let key: string | undefined = Cookies.get('key');
             getResponse(key).then(r => null);
 
         }
+    }*/
+/*
+    const checkAuth = async () => {
+        if (localStorage.getItem('authKey')) {
+            // @ts-ignore
+            const response = await axios.get('/api/user', {withCredentials: false, headers:{'key': localStorage.getItem('authKey').toString()}})
+            setAuth(await response.data.userResponse);
+            console.log(JSON.stringify(auth) + " - checkAuth json(auth)");
+            // @ts-ignore
+            console.log(JSON.stringify(localStorage.getItem('authKey')) + " - checkAuth json(localStorage.getItem)")
+        }
     }
 
 
+
+    useEffect(() => {checkAuth()}, []);*/
 
     return (
         <AuthContext.Provider value={[auth, setAuth]}>
