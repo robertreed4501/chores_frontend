40941eac90e8f726183bd1cde3ac4845e9717e58
robertreed4501 <robertreed4501@gmail.com>
2022-11-14 18:54:27 -0500

    switched console.log and alert for error responses to make more sense

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -8,10 +8,13 @@ import {AuthContext} from "../context/AuthProvider";
 import {AdminContext} from "../context/AdminProvider";
 import {useNavigate} from "react-router-dom";
 import Cookies from "js-cookie";
+import bcrypt from "bcryptjs";
 
 const LOGIN_URL = '/login';
 
 export const LoginModal = () => {
+
+
 
     const [show, setShow] = useState(false);
     // @ts-ignore
@@ -31,6 +34,7 @@ export const LoginModal = () => {
     const handleSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
 
+
         try{
             const response = await axios.post(
                 LOGIN_URL,
@@ -43,25 +47,28 @@ export const LoginModal = () => {
             );
             console.log(JSON.stringify(response));
             if (response.data.error){
-                console.log(response.data.error)
-            }else{
+                alert(response.data.error)
+            }//else{
                 console.log("logged in with token:" + response.data.userResponse.key);
                 setAuth(response.data.userResponse);
+                localStorage.setItem('authKey', response.data.userResponse.key);
+                localStorage.setItem('loggedIn', String(true));
+                console.log(localStorage.getItem('auth') + " - auth from local storage in login modal");
                 /*setTimeout(() => 500);
                 console.log(auth.id + " auth after setAuth")*/
                 console.log(response.data.userResponse.groupId + " response.data.userResponse")
                 setUser('');
                 setPassword('');
                 setSuccess(true);
-                Cookies.set('key', response.data.userResponse.key, { expires: 7});
+                Cookies.set('key', response.data.userResponse.key, { expires: 7, sameSite: "none"});
                 console.log(Cookies.get('key') + "  - from login.tsx")
                 response.data.userResponse.role === "USER" ?
                     navigate("/user", {replace: false}) :
                     navigate("/dashboard")
-            }
+            //}
 
         }catch (err){
-            alert(err)
+            console.log(err)
         }
         //success ? navigate("/user", {replace: true}) : navigate("/user", {replace: true});
 
